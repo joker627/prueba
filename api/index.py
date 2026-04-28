d23325c1408f1c9470d04b3b92ef1623e8864d25
Manuel-Dev <73049543+joker627@users.noreply.github.com>
2026-04-28 08:04:50 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
 from mangum import Mangum
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# Configuración de CORS
+origins = [
+    "http://localhost",  # Para tu entorno de desarrollo local
+    "http://localhost:3000",
+    "http://127.0.0.1:3000", # Si usas React u otros frameworks con puerto específico
+    "https://tudominio.com"  # Cambia por el dominio de tu frontend
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Permitir dominios
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos los métodos HTTP
+    allow_headers=["*"],  # Permitir todos los headers
+)
+
+# Datos de ejemplo
 datos = [
     {"id": 1, "nombre": "Juan", "edad": 25, "pais": "Colombia"},
     {"id": 2, "nombre": "Ana", "edad": 30, "pais": "México"},
@@ -17,7 +35,6 @@ datos = [
 @app.get("/")
 def home():
     return {"datos": datos}
-
 
 # handler para serverless
 handler = Mangum(app)
